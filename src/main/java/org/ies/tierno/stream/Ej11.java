9c0ad1bb343fcbe00b8817629046c87d412c50d2
Paula781 <paulavilches81@hotmail.com>
2024-04-11 10:57:15 +0200

    Ejercicios 11 y 12 acabados

--- src/main/java/org/ies/tierno/stream/Ej11.java
+++ src/main/java/org/ies/tierno/stream/Ej11.java
@@ -1,5 +1,9 @@
 package org.ies.tierno.stream;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
 public class Ej11 {
     /*
 
@@ -9,6 +13,16 @@ Si la lista está vacía devuelve null.
      */
 
     public static void main(String[] args) {
+        List<Double> numbers= List.of(1.8,2.7,8.0,9.2,10.0,20.5,50.0);
+        Double minNumber= min(numbers);
+        System.out.println("El minimo de los numeros de la lista es: "+minNumber);
+    }
 
+    public static Double min(List<Double> numbers){
+        Optional<Double> result= numbers
+                .stream()
+                .min(Double::compareTo); //Encuentra el minimo
+
+        return  result.orElse(null); //Devuelve el min, sino null
     }
 }
--- src/main/java/org/ies/tierno/stream/Ej12.java
+++ src/main/java/org/ies/tierno/stream/Ej12.java
@@ -1,5 +1,9 @@
 package org.ies.tierno.stream;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Ej12 {
     /*
     Un método junction(List<Double> numbers1 ,
@@ -9,6 +13,20 @@ public class Ej12 {
      */
 
     public static void main(String[] args) {
+        List<Double> numbers1 = List.of(1.5, 2.5, 3.5, 4.5, 5.5);
+        List<Double> numbers2 = List.of(3.5, 4.5, 5.5, 6.5, 7.5);
+        List<Double> commonNumbers = junction(numbers1, numbers2);
+        System.out.println("Números en común entre las dos listas: " + commonNumbers);
 
+    }
+    public static List<Double> junction(List<Double> numbers1, List<Double> numbers2) {
+        Set<Double> set1 = numbers1
+                .stream()
+                .collect(Collectors.toSet()); // Convertir la primera lista a un conjunto
+
+        return numbers2
+                .stream()
+                .filter(set1::contains) // Filtrar los números de la segunda lista que están en el conjunto de la primera lista
+                .collect(Collectors.toList()); // números comunes en una nueva lista
     }
 }
